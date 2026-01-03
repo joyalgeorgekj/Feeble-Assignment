2f55a6268c79af43b6cfce713fbdb81b2e6004be
Joyal George K J <joyalgeorgekj@gmail.com>
2026-01-04 04:51:36 +0530

    Initial delay

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
+import { useEffect, useState } from "react";
 import Background from "./components/Background";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Scroll from "./components/Scroll";
 
 function App() {
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => setLoading(true), 1000);
+    });
     return (
         <div className="relative w-full h-screen overflow-hidden">
-            <Header />
-            <Main />
-            <Scroll />
-            <Background />
+            {loading && (
+                <>
+                    <Header />
+                    <Main />
+                    <Scroll />
+                    <Background />
+                </>
+            )}
         </div>
     );
 }

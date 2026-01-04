94099dca64c46945db9a9ca449edec0a12d3c32a
Joyal George K J <joyalgeorgekj@gmail.com>
2026-01-04 18:53:29 +0530

    Removed Loading state

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -1,18 +1,18 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import Background from "./components/Background";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Scroll from "./components/Scroll";
 
 function Landing() {
-    const [loading, setLoading] = useState(false);
+    // const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        setTimeout(() => setLoading(true), 1000);
-    });
+    // useEffect(() => {
+    //     setTimeout(() => setLoading(true), 1000);
+    // });
     return (
         <div className="relative w-full h-screen overflow-hidden">
-            {loading && (
+            {/* loading && */(
                 <>
                     <Header />
                     <Main />
